cf081b73828ef4a72927ca66480f9a1a783b8273
Vladislav <Vladislavzikstudy@gmail.com>
2022-12-02 15:29:46 +0300

    add Measure Model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class DataTimeModelMixin(models.Model):
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class TitleDescriptionModelMixin(models.Model):
+    title = models.CharField(max_length=25, unique=True)
+    description = models.TextField(max_length=1000, blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Measure(TitleDescriptionModelMixin, DataTimeModelMixin):
+    def __str__(self):
+        return f'{self.title}'
