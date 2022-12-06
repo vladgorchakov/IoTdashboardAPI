e983bde4fb6462a54b5d7bdce0261c4cf3598b3b
Vladislav <Vladislavzikstudy@gmail.com>
2022-12-06 16:24:03 +0300

    add token auth

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include, re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/drf-auth/', include('rest_framework.urls')),
-    re_path(r'^auth/', include('djoser.urls.authtoken')),
     path('api/v1/', include('dashboard.urls')),
+    path('api/v1/auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
 
