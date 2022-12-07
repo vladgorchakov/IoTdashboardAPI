8b7e7c04e357d9bb349228061d9919be49da2fa4
Vladislav <Vladislavzikstudy@gmail.com>
2022-12-07 16:06:37 +0300

    create throttling for project and MonitoringViewSet

--- dashboard/throttles.py
+++ dashboard/throttles.py
@@ -2,7 +2,8 @@ from rest_framework import throttling
 
 
 class UserMonitoringCreateRateThrottle(throttling.UserRateThrottle):
-    rate = '10/minute'
+    rate = '120/minute'
+    def allow_request(self, request, view):
 
 
 class UserMonitoringRateThrottle(throttling.UserRateThrottle):
